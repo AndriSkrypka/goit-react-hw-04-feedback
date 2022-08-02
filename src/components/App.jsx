02c50917725e2e794989176509431237b6d44e23
AndriiSkrypka <andriskrupka25011994@icloud.com>
2022-08-02 14:01:07 +0300

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import { Component } from 'react';
+import Feedbacks from './Feedbacks/Feedbacks';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+
+
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  protectTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  
+  protectPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good * 100) / this.protectTotalFeedback());
+  };
+
+
+  handlerButton = elem => {
+    const { name } = elem.target;
+    this.setState(ps => {
+      return { [name]: ps[name] + 1 };
+    });
+  };
+
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <Feedbacks
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handlerButton}
+          ></Feedbacks>
+        </Section>
+        <Section title="Statistics">
+          {!this.protectTotalFeedback() ? (
+            <Notification message="There is no feedback"></Notification>
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.protectTotalFeedback()}
+              positivePercentage={this.protectPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
+
+
+export { App };
